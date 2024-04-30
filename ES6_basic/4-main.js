3427b38ebad94c39dc903bd4578ae4edce6785f4
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-04-30 19:51:16 +0000

    add main file for rest-parameter

---
+++ ES6_basic/4-main.js
@@ -0,0 +1,5 @@
+bob@dylan:~$ cat 4-main.js
+import returnHowManyArguments from './4-rest-parameter.js';
+
+console.log(returnHowManyArguments("one"));
+console.log(returnHowManyArguments("one", "two", 3, "4th"));
