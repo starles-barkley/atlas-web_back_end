d8be6fbb9092e2f3e1dcff133b4b67509e2ca300
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-02 19:35:45 +0000

    incorporate for ... of

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
-
-  return array;
+  return newArray;
 }
