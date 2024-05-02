466980757a79d71746297c68cc2536cdf1483d7c
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-02 22:06:30 +0000

    add space between class members

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,6 @@
 class HolbertonCourse {
   constructor(name, length, students) {
+    
     this._name = typeof name === 'string' ? name : '';
     this._length = typeof length === 'number' ? length : 0;
     this._students = Array.isArray(students) ? students : [];
