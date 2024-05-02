e8a44c6c9e9f5284ebc6bc729be801bd8d64a0c1
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-02 22:26:55 +0000

    add blank line between class members?

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,4 +1,5 @@
 export default class HolbertonCourse {
+  
   constructor(name, length, students) {
     this._name = typeof name === 'string' ? name : '';
     this._length = typeof length === 'number' ? length : 0;
