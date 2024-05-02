797954dd2cc77dcfb7540eb2941dd6c22091d751
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-02 21:40:46 +0000

    create holbertoncourse class and define the types of the attributes

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,8 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = typeof name === 'string' ? name : '';
+    this._length = typeof length === 'number' ? length : 0;
+    this._students = Array.isArray(students) ? students : [];
+  }
+  
+}
