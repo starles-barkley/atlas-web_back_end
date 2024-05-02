65b99c12bdbeb2edb7bea48a2a0ce6603cc3cf8d
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-02 22:29:50 +0000

    fix spacing issues

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,10 +1,11 @@
 export default class HolbertonCourse {
-  
+
   constructor(name, length, students) {
     this._name = typeof name === 'string' ? name : '';
     this._length = typeof length === 'number' ? length : 0;
     this._students = Array.isArray(students) ? students : [];
   }
+  
   get name() {
     return this._name;
   }
