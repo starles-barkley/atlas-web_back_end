8206ab1493c04469bbf82af3a12286b990043852
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-06 10:33:49 +0000

    set attributes

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -3,5 +3,33 @@ export default class Car {
     this.brand = brand;
     this.motor = motor;
     this.color = color;
+  } 
+
+  set brand(val) {
+    this._brand = val;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set motor(val) {
+    this._motor = val;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set color(val) {
+    this._color = val;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
   }
 }
