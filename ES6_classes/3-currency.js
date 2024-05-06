5d9dcafef54a42095730b484583036a583abf152
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-06 07:12:57 +0000

    create getters and setters

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,5 +1,24 @@
-export default class Currency {
-  constructor(code, string)
-  this._code = code;
-  this._name = name;
+class Currency {
+  constructor(code, name) {
+      this._code = code;
+      this._name = name;
+  }
+
+  get code() {
+      return this._code;
+  }
+  set code(value) {
+      this._code = value;
+  }
+
+  get name() {
+      return this._name;
+  }
+  set name(value) {
+      this._name = value;
+  }
+
+  displayFullCurrency() {
+      return `${this._name} (${this._code})`;
+  }
 }
