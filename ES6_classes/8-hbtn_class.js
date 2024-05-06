c765ddf2ae7bc32d378e7825939acd280c318ee4
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-06 08:53:16 +0000

    create getter and setters for holbertonclass and add valueof method

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -3,5 +3,36 @@ export default class HolbertonClass {
     this.size = size;
     this.location = location
   }
-  
+
+  toString() {
+    return this.location;
+  }
+
+  valueOf() {
+    return this.size;
+  }
+
+  set size(val) {
+    if (typeof (val) === 'number') {
+      this._size = val;
+    } else {
+      throw TypeError('size must be a number');
+    }
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set location(val) {
+    if (typeof (val) === 'string') {
+      this._location = val;
+    } else {
+      throw TypeError('location must be a string');
+    }
+  }
+
+  get location() {
+    return this._location;
+  }
 }
