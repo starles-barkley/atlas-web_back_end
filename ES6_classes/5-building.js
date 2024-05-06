c22cf071956ef76b380a3f2a94efa6fde5c8f9cb
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-06 07:59:18 +0000

    add getter and setter for sqft

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -7,3 +7,16 @@ export default Building {
     }
     this.sqft = sqft;
   }
+
+  set sqft(val) {
+    if (typeof (val) === 'number') {
+      this._sqft = val;
+    } else {
+      throw TypeError('sqft must be a number');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
