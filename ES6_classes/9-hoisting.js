caf0251f9fb5b050b49f8e243e015ee6d5d50c7b
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-06 10:48:19 +0000

    fix spacing issues on task 9

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,4 +1,3 @@
-
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -16,7 +15,6 @@ export class HolbertonClass {
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
