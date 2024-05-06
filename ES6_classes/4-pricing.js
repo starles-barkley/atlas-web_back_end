622a833a1121384bdca3b71e6fe6097e51816b6b
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-06 07:27:39 +0000

    create pricing class and constructor and setter and getter for amount

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,19 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set amount (val) {
+    if (typeof (val) ===  'number') {
+      this._amount = val;
+    } else {
+      throw TypeError("Amount must be a number");
+    }
+  }
+
+  get amount() {
+    return this._amount;
+  }
