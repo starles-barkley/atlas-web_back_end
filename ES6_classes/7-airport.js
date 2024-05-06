02d9fb5ea4b5e8f3d25e55753f3cdc14b164c202
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-06 08:38:43 +0000

    add getter and setter for airport class;

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,30 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  set name(val) {
+    if (typeof (val) === 'string') {
+      this._name = val;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(val) {
+    if (typeof (val) === 'string') {
+      this._code = val;
+    } else {
+      throw TypeError('Code must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+}
