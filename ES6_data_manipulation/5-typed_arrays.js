b62de787d20f49c57f9439ffe88197fcdded71f2
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-07 23:43:45 +0000

    add const buffer and dataview to return dataview

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -2,4 +2,9 @@ export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
+  
+  const buffer = new ArrayBuffer(length);
+  const dataView = new DataView(buffer);
+
+  return dataView;
 }
