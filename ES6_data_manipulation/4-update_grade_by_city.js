9867162f4ac6d320484e2a23bb66bd24ba512882
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-07 03:27:50 +0000

    create filter and map

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,4 +2,14 @@ export default updateStudentGradeByCity(students, city, newGrades) {
   if (!Array.isArray(students)) {
     return [];
   }
+
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const studentGrade = updatedGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: studentGrade ? studentGrade.grade : 'N/A',
+      };
+    });
 }
