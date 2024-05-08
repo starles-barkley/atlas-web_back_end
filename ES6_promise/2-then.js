085b972d16d32b7d2653d6e4d6203f491ab6fb06
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-08 02:06:47 +0000

    fix spacing issue

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-    return promise.then(() => {
+  return promise.then(() => {
     console.log('Got a response from the API');
     return ({ status: 200, body: 'success' });
   }).catch(() => {
