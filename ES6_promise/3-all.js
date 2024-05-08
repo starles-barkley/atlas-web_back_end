580d693a505f4bd406a3796f066d387c33028157
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-08 02:10:53 +0000

    fix indentations

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,10 +2,10 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-  .then(([uploadPhoto, createUser]) => {
-    console.log(`${uploadPhoto.body} ${createUser.firstName} ${createUser.lastName}`);
-  })
-  .catch(() => {
-    console.log('Signup system offline');
-  });
+    .then(([uploadPhoto, createUser]) => {
+      console.log(`${uploadPhoto.body} ${createUser.firstName} ${createUser.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
