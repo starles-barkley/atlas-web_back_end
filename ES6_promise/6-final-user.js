3c50c7aeb47bf11c45b6943b93f43597cd2185ea
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-08 03:05:45 +0000

    actually push my changes;

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,4 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promises).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
+  })));
 }
