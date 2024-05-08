eba89f1b2453549ff6c5fbcab69dc0bebcb41103
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-05-08 00:04:34 +0000

    convert set to an arrayand incorporate filter and map and join

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,4 +2,9 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') {
     return '';
   }
+
+  return Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
 }
