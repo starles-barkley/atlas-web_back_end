0d3951a383940cf7461aa31217144135f1e99b16
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-08-31 04:52:24 +0000

    create type annotated function for concatenating strs

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Defines a type-annotated function to concatenate two strings.'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Returns str1 & str2 as a concatenated str'''
+    return str1 + str2
