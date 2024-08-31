41a13e270d2c2bc8825abdfb4c3c244c74ae69d2
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-08-31 21:06:21 +0000

    fix documentation for pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-'''This script defines an asynchronous routine `wait_n` that spawns the `wait_random`
-coroutine multiple times and returns a list of delay times in ascending order.'''
+'''This script defines an asynchronous routine `wait_n`
+that spawns the `wait_random`coroutine multiple times
+and returns a list of delay times in ascending order.'''
 
 import asyncio
 from typing import List
@@ -8,8 +9,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''Asynchronous routine that spawns `wait_random` n times with the
-    specified max_delay. Returns a list of all the delays in ascending order.'''
+    '''Asynchronous routine that spawns `wait_random'
+    n times with the specified max_delay. Returns a list
+    of all the delays in ascending order.'''
 
     tasks = [wait_random(max_delay) for _ in range(n)]
 
