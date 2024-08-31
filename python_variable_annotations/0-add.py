d84206e217e2213a54a775886f2d0f3b723a81fc
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-08-31 04:43:12 +0000

    add function documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+'''Defines a type-annotated function to add two float numbers.'''
+
 def add(a: float, b: float) -> float:
   '''Adds two floating-point numbers and returns their sum.'''
     return a + b
