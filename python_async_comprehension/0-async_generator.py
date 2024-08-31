dcfb8dbb0bff5da6a9406c9d01c216e994d02737
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-08-31 22:09:52 +0000

    add function annotations

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,6 +8,7 @@ from typing import AsyncGenerator
 async def async_generator() -> AsyncGenerator[float, None]:
     '''Coroutine that asynchronously generates
     10 random numbers between 0 and 10.'''
+
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
