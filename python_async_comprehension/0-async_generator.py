55f31a9df42b0f925b58a9f2b498726fb2718247
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-08-31 22:05:31 +0000

    draft async generator script

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''This script defines an async generator that
+yields random numbers between 0 and 10.'''
+import asyncio
+import random
+from typing import AsyncGenerator
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    '''Coroutine that asynchronously generates
+    10 random numbers between 0 and 10.'''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
