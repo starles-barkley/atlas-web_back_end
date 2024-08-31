d607bf1980dfd06988b0d56bc62b225e366663d5
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-08-31 21:00:24 +0000

    modify script to import previous script

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,22 +4,15 @@ coroutine multiple times and returns a list of delay times in ascending order.''
 
 import asyncio
 from typing import List
-from previous_script import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ Asynchronous routine that spawns `wait_random` n times
-    with the specified max_delay. Returns a list of all the delays
-    in ascending order."""
+    '''Asynchronous routine that spawns `wait_random` n times with the
+    specified max_delay. Returns a list of all the delays in ascending order.'''
 
-    delays = []
-    for _ in range(n):
-        delay = await wait_random(max_delay)
-        for i, existing_delay in enumerate(delays):
-            if delay < existing_delay:
-                delays.insert(i, delay)
-                break
-        else:
-            delays.append(delay)
-    
+    tasks = [wait_random(max_delay) for _ in range(n)]
+
+    delays = [await task for task in asyncio.as_completed(tasks)]
+
     return delays
