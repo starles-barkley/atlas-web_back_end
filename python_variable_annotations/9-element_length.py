4e489c71040050373dd8875df5d3349d7209298b
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-08-31 06:27:00 +0000

    import Iterable from typing

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 '''Defines a type-annotated function to
 return lengths of iterable sequences.'''
-from typing import List, Tuple, Any
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: List[Any]) -> List[Tuple[Any, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''Return a list of tuples with each sequence and its length.'''
     return [(i, len(i)) for i in lst]
