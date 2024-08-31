ea742842ebef77ea51aebd91890c234a4a95bc11
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-08-31 05:19:34 +0000

    create annotated type function that returns str version of a float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Defines a type-annotated function to return the 
+string representation of a float.'''
+
+
+def to_str(n: float) -> str:
+    '''Returns the string representation of a float'''
+    return str(n)
