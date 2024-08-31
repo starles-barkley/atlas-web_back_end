a22077dbb2dc229ef09f149a36664fa4269a4b30
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-08-31 22:15:02 +0000

    import generator instead of asyncgenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,7 +3,7 @@
 yields random numbers between 0 and 10.'''
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
