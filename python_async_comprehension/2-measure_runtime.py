c0513bf4f79611fa9f4563de917444853c9ba43d
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-08-31 22:41:18 +0000

    create async runtime function

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""This script defines an asynchronous function
+`measure_runtime` that measures the total execution
+time for running the `async_comprehension` coroutine
+four times concurrently."""
+
+import asyncio
+import time
+from typing import List
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measures the total runtime of executing the
+    `async_comprehension` coroutine four times concurrently."""
+    start_time = time.perf_counter()
+
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+
+    total_time = time.perf_counter() - start_time
+    return total_time
