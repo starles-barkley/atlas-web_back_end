00e1d2e018a3e1b26e82e21e7e3278e8694d666b
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-08-31 04:58:18 +0000

    create annotated type function that returns floor of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Defines a type-annotated function to return the floor of a float.'''
+import math
+
+
+def floor(n: float) -> int:
+    '''Returns the floor of a float'''
+    return math.floor(n)
