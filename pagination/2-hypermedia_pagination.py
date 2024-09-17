f35aaa57e3687fa28394f57fb5b97799d80fd0b5
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-17 19:19:34 +0000

    create hypermedia script and add get_hyper function with documentation

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''Simple Pagination Module'''
+import csv
+import math
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''Retrieve info from the csv'''
+        pass
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''A different method I'll add more doc for'''
+        pass
