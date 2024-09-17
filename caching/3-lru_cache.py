c6087c50d1e79365cde3f8b56f2fe171ed4c0836
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-17 04:37:50 +0000

    pycodestyle

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -19,10 +19,10 @@ class LRUCache(BaseCaching):
                 lru_key = self.access_order.pop(0)
                 del self.cache_data[lru_key]
                 print(f"DISCARD: {lru_key}")
-            
+
             if key in self.cache_data:
                 self.access_order.remove(key)
-            
+
             self.cache_data[key] = item
             self.access_order.append(key)
 
@@ -30,7 +30,7 @@ class LRUCache(BaseCaching):
         '''Retrieve an item from the cache'''
         if key is None or key not in self.cache_data:
             return None
-        
+
         self.access_order.remove(key)
         self.access_order.append(key)
         return self.cache_data[key]
