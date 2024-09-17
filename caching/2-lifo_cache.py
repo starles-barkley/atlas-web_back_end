8a75af0ed86cf4cd6eeb9934928e1bbd0650c2ce
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-17 04:06:46 +0000

    add LIFO class methods

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -8,3 +8,19 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         super().__init__()
         self.last_key = None
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            if key not in self.cache_data and len(
+              self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if self.last_key is not None:
+                    del self.cache_data[self.last_key]
+                    print(f"DISCARD: {self.last_key}")
+            
+            self.cache_data[key] = item
+            self.last_key = key
+
+    def get(self, key):
+        if key is None:
+            return None
+        return self.cache_data.get(key)
