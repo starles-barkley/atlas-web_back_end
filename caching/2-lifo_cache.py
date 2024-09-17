77d7d8a5d3b070ff493ee7aaec0a214b24d07a8a
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-17 04:10:34 +0000

    fix pycodestyle

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -14,7 +14,8 @@ class LIFOCache(BaseCaching):
     def put(self, key, item):
         '''Add an item to the cache using the LIFO algorithm'''
         if key is not None and item is not None:
-            if key not in self.cache_data and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key not in self.cache_data and len(
+              self.cache_data) >= BaseCaching.MAX_ITEMS:
                 if self.last_key is not None:
                     del self.cache_data[self.last_key]
                     print(f"DISCARD: {self.last_key}")
