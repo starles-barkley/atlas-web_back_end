57d25e7127b3821c596a0e11601abe05adc4d3be
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-17 03:25:46 +0000

    pycodestyle

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -3,16 +3,17 @@
 
 BaseCaching = __import__('base_caching').BaseCaching
 
-class BasicCache(BaseCaching):
-  '''Defines a caching system with no item limit & 
-  inherits from BaseCaching.'''
-  def put(self, key, item):
-    """Assigns the item to the cache_data dictionary using the key."""
-    if key is not None and item is not None:
-      self.cache_data[key] = item
 
-  def get(self, key):
-    """Returns the value linked to the key in cache_data."""
-    if key is None:
-        return None
-    return self.cache_data.get(key)
+class BasicCache(BaseCaching):
+    '''Defines a caching system with no item limit & 
+    inherits from BaseCaching.'''
+    def put(self, key, item):
+      """Assigns the item to the cache_data dictionary using the key."""
+      if key is not None and item is not None:
+        self.cache_data[key] = item
+
+    def get(self, key):
+      """Returns the value linked to the key in cache_data."""
+      if key is None:
+          return None
+      return self.cache_data.get(key)
