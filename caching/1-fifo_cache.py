ebaafb86592dbc4d0c56446de58a2c491078bdcb
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-17 03:56:27 +0000

    add methods to FIFOCache

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -10,3 +10,21 @@ class FIFOCache(BaseCaching):
         """Initialize the FIFO cache."""
         super().__init__()
         self.keys_order = []
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            if key not in self.cache_data and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                first_key = self.keys_order.pop(0)
+                del self.cache_data[first_key]
+                print(f"DISCARD: {first_key}")
+            
+            if key in self.cache_data:
+                self.keys_order.remove(key)
+            
+            self.keys_order.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None:
+            return None
+        return self.cache_data.get(key)
