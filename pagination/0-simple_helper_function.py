0556b4910f3b1bd7cd3bd2720420972896599e9d
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-17 18:33:10 +0000

    add documentation to the function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 
 
 def index_range(page, page_size):
+    '''Determine the start and end index for pagination'''
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
