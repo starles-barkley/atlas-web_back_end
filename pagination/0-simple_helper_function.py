87e5391858773bca2d444955cf00ae48b86097bd
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-17 04:51:02 +0000

    create function index range

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Simpler Helper Module'''
+
+
+def index_range(page, page_size):
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
