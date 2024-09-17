f0ee8bfb33874c79243411feca531873a2126338
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-17 21:08:14 +0000

    add documentation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+'''This module contains a function for filtering log messages'''
 import re
 
 def filter_datum(fields, redaction, message, separator):
+    '''Obfuscates fields in a log message'''
     pattern = f"({'|'.join(fields)})=([^\\{separator}]*)"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
