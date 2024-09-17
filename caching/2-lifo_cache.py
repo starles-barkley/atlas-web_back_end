9f1a2a08bb75a78b8b15d32d33339e373ae536b1
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-17 04:11:01 +0000

    remove whitespace

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -19,7 +19,7 @@ class LIFOCache(BaseCaching):
                 if self.last_key is not None:
                     del self.cache_data[self.last_key]
                     print(f"DISCARD: {self.last_key}")
-            
+
             self.cache_data[key] = item
             self.last_key = key
 
