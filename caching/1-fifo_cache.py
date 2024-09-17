a016ba7e89e11e1aa3fbe7fd68788dc84a488f7a
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-17 04:00:02 +0000

    pycodestyle

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -12,21 +12,22 @@ class FIFOCache(BaseCaching):
         self.keys_order = []
 
     def put(self, key, item):
-        """Add an item to the cache using the FIFO algorithm."""
+        """Add an item to the cache using the FIFO algorithm"""
         if key is not None and item is not None:
-            if key not in self.cache_data and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key not in self.cache_data 
+            and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 first_key = self.keys_order.pop(0)
                 del self.cache_data[first_key]
                 print(f"DISCARD: {first_key}")
-            
+
             if key in self.cache_data:
                 self.keys_order.remove(key)
-            
+
             self.keys_order.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
-        """Retrieve an item from the cache."""
+        """Retrieve an item from the cache"""
         if key is None:
             return None
         return self.cache_data.get(key)
