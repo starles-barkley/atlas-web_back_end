47d253cc559712dc4b4d940e81551661307177de
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-18 18:39:18 +0000

    update class to acept fields as an argument

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
-'''This module contains a function for filtering log messages'''
+'''This module filters log messages'''
 import re
 from typing import List
 import logging
 
-
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
+    """ Redacting Formatter class """
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+        original_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
 
 def filter_datum(fields: List[str],
                  redaction: str,
