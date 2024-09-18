95e70cfa901e642ed7a7111a002c8cff6bd5d43f
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-18 19:33:49 +0000

    add get_db function

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,10 @@
 import re
 from typing import List
 import logging
+import os
+import mysql.connector
+from mysql.connector.connection import MySQLConnection
+
 
 PII_FIELDS = ('phone', 'name', 'ssn', 'password', 'email')
 
@@ -49,3 +53,18 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> MySQLConnection:
+    '''Returns a connection to the MySQL database'''
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    database = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
+    )
