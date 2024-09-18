fdcfec783ceaa6277b1c46aa49e9756e1ccab673
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-18 22:13:49 +0000

    create auth script

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Authentication module for the API. """
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """ Template class for authentication system. """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Determines if the path requires authentication. 
+            For now, returns False.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Retrieves the authorization header from the request.
+            For now, returns None.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Retrieves the current user from the request.
+            For now, returns None.
+        """
+        return None
