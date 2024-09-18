72eedbdde50cbccb6811b9e6d59e47cdcf849699
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-18 21:44:36 +0000

    add error handling for 403

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized():
     """Unathorized error"""
     abort(401)
+
+
+@app_views.route('/api/v1/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden_endpoint():
+    """ Endpoint to trigger 403 error """
+    abort(403)
