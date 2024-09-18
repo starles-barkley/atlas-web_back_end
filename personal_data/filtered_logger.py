cd0c9b636ccc3cab60279bbe08f875ab72073f73
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-18 18:28:55 +0000

    fix function signature

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,8 +4,10 @@ import re
 from typing import List
 
 
-def filter_datum(
-    fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     '''Obfuscates fields in a log message'''
     pattern: str = f"({'|'.join(fields)})=([^\\{separator}]*)"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
