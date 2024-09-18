b33a7d1105f560d5316d2e799d11541e7fa046b3
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-18 19:10:57 +0000

    remove whitespace from documentation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ import logging
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class 
+    """ Redacting Formatter class
     """
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
