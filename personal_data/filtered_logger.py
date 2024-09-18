bc49c4bb68f5d67e43ca85008fac07c1f354ecf4
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-18 19:27:30 +0000

    modify get_logger function to return a tuple

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -37,13 +37,11 @@ def filter_datum(fields: List[str],
 
 def get_logger() -> logging.Logger:
     '''Returns a configured logger for user data'''
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
+    hdlr = logging.StreamHandler()
+    hdlr.setFormatter(RedactingFormatter(PII_FIELDS))
 
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
-
-    logger.addHandler(stream_handler)
-
-    return logger
+    user_data = logging.getLogger('user_data')
+    user_data.setLevel(logging.INFO)
+    user_data.addHandler(hdlr)
+    user_data.propagate = False
+    return user_data
