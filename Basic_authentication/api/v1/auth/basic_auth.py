1d441da5d7932f7adb15e7f9bf27eecd45f4183b
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-20 19:34:47 +0000

    PYCODESTYLE!

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ BasicAuth module for handling basic authentication """
 
-
 from api.v1.auth.auth import Auth
+
 
 class BasicAuth(Auth):
     """ BasicAuth class that inherits from Auth """
