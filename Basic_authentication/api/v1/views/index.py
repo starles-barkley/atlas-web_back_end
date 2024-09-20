cb218dc9f6da98698bd819b9071c08bb4c719c9a
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-20 19:09:07 +0000

    update init package and index to ensure all route ports are defined

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,51 +1,22 @@
 #!/usr/bin/env python3
-"""
-Route module for the API
-"""
-from os import getenv
+""" Module of Index views """
+
+from flask import jsonify, abort
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
 
-app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return: the status of the API
+    """
+    return jsonify({"status": "OK"})
 
-auth = None
-AUTH_TYPE = getenv('AUTH_TYPE')
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized():
+    """ Unauthorized error """
+    abort(401)
 
-if AUTH_TYPE == 'auth':
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler """
-    return jsonify({"error": "Not found"}), 404
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized handler """
-    return jsonify({"error": "Unauthorized"}), 401
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden handler """
-    return jsonify({"error": "Forbidden"}), 403
-
-@app.before_request
-def before_request():
-    """ Method to handle before each request. """
-    if auth is None:
-        return
-
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden_endpoint():
+    """ Endpoint to trigger 403 error """
+    abort(403)
