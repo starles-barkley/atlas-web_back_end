cd46f02c44f0eb52f55668e19c5e9ae783a7681e
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-20 20:09:08 +0000

    test theory

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -65,6 +65,6 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
 
-        user_email,
+        user_email =  decoded_base64_authorization_header.split(':', 1)
         password = decoded_base64_authorization_header.split(':', 1)
         return user_email, password
