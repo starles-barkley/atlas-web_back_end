cc089fafcd569be500e21a639f5b20c28e3bc990
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-20 20:23:36 +0000

    create user_object_from_credentials method and fix pycodestyle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -68,3 +69,33 @@ class BasicAuth(Auth):
         user_email, password = decoded_base64_authorization_header.split(
           ':', 1)
         return user_email, password
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        Returns the User instance based on email and password.
+
+        Args:
+            user_email (str): The user's email.
+            user_pwd (str): The user's password.
+
+        Returns:
+            TypeVar('User'): The User instance if credentials
+            are valid, otherwise None.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+        if not users or len(users) == 0:
+            return None
+
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
