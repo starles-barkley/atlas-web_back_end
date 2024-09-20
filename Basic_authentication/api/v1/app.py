232709ed4ecb949c28ce7cfe45ce609df7457078
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-20 19:19:40 +0000

    incorporate BasicAuth class into app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -3,20 +3,23 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS
+from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
 
-if AUTH_TYPE == 'auth':
+if AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
@@ -41,7 +44,7 @@ def forbidden(error) -> str:
 
 @app.before_request
 def before_request():
-    """ Method to handle before each request. """
+    """ Method to handle before each request """
     if auth is None:
         return
 
