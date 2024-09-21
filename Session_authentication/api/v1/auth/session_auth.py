d22ff2e51a9f766854042558472773f5d23a4c42
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-21 23:14:32 +0000

    add return to create_session method

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,4 @@ class SessionAuth(Auth):
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id.update({session_id: user_id})
+        return session_id
