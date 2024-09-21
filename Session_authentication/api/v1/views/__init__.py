b392e58d6089d8d137dcd2f92d19232774400395
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-21 23:47:53 +0000

    fix typos

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" DocDocDocDocDocDoc
+""" Init Package
 """
 from flask import Blueprint
 
--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python3
-""" Module for Session Authentication routes
+""" Module for Session Auth routes
 """
-from flask import jsonify, request, abort
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models.user import User
+from os import getenv
 
-@app_views.route('/auth_session/login/', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
     """ Handles the login for session authentication """
-    
-    email = request.form.get('email')
-    password = request.form.get('password')
 
-    if not email:
+    email = request.form.get('email')
+    if email is None or email == "":
         return jsonify({"error": "email missing"}), 400
-    if not password:
+
+    password = request.form.get('password')
+    if password is None or password == "":
         return jsonify({"error": "password missing"}), 400
 
-    user = User.search(email=email)
-    if user is None:
+    user = User.search({'email': email})
+    if len(user) == 0:
         return jsonify({"error": "no user found for this email"}), 404
 
-    if not user.is_valid_password(password):
+    if not user[0].is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
-    session_id = auth.create_session(user.id)
-
-    response = jsonify(user.to_json())
-    session_name = getenv("SESSION_NAME", "_my_session_id")
-    response.set_cookie(session_name, session_id)
-
+    sesh_id = auth.create_session(user[0].id)
+    response = jsonify(user[0].to_json())
+    response.set_cookie(getenv('SESSION_NAME'), sesh_id)
     return response
