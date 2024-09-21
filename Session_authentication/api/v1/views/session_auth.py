292ff9d3da4a123489770295ce8457fcacd1582c
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-21 23:52:51 +0000

    add logout method to session_auth

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -31,3 +31,18 @@ def login():
     response = jsonify(user[0].to_json())
     response.set_cookie(getenv('SESSION_NAME'), sesh_id)
     return response
+
+
+@app_views.route(
+    '/auth_session/logout/',
+    methods=['DELETE'],
+    strict_slashes=False)
+
+
+def logout():
+    """ Deletes the user session / logout """
+
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
