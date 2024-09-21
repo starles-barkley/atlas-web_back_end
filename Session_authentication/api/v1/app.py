a49a8cd6d86311cd4336f271615ff343245ba2f4
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-21 22:55:56 +0000

    fix line that is too long

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -48,7 +48,9 @@ def before_request():
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
 
