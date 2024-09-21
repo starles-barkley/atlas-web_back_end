1ce3bf9f22be14a7811d543f72c376cadbb17f35
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-21 23:36:41 +0000

    add section to before_request to abort if no cookie or header

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -5,8 +5,10 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS
 import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.session_auth import SessionAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,15 +19,8 @@ auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
 
 if AUTH_TYPE == 'auth':
-    from api.v1.auth.auth import Auth
     auth = Auth()
-
-elif AUTH_TYPE == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-
 elif AUTH_TYPE == 'session_auth':
-    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
 
@@ -49,24 +44,27 @@ def forbidden(error) -> str:
 
 @app.before_request
 def before_request():
-    """ Method to handle before each request. """
+    """ Method to handle actions before each request """
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/'
-                      '/api/v1/auth_session/login/']
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
+
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
 
-    if auth.current_user(request) is None:
-        abort(403)
-
     request.current_user = auth.current_user(request)
+    if request.current_user is None:
+        abort(403)
 
 
 if __name__ == "__main__":
