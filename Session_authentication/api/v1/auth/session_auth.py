8cbf7ae09ecee84056b2764fa68cbe979475b18f
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-21 23:12:34 +0000

    fix documentation for session_auth

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,28 +1,22 @@
 #!/usr/bin/env python3
-""" Module for Session Authentication """
-
+""" Module for Session Authorization
+"""
 from flask import request
-import uuid
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
-    """ SessionAuth class that manages session authentication """
+    """ Session Authorization class
+    """
 
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """ create a session id for a user
         """
-        Creates a Session ID for a given user_id.
 
-        Args:
-            user_id (str): The ID of the user for whom the
-            session ID is being created.
-
-        Returns:
-            str: The session ID, or None if user_id is invalid.
-        """
-        if user_id is None or not isinstance(user_id, str):
+        if user_id is None or type(user_id) is not str:
             return None
-
-        session_id =
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id.update({session_id: user_id})
