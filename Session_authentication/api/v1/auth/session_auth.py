1de0d2c15212a91527dc80f0ef8f145ee7ef663f
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-21 23:38:58 +0000

    add method to return user based on user id

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -48,3 +48,19 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Return a User instance based on a cookie value """
+
+        if request is None:
+            return None
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+
+        return User.get(user_id)
