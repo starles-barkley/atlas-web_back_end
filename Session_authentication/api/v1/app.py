c9b39941c2e49cdda5506971ac2ce34d119eaa3c
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-21 23:00:12 +0000

    create session_auth class and add to app.py

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -3,26 +3,30 @@
 Route module for the API
 """
 from os import getenv
-from flask import Flask, jsonify, abort, request
-from flask_cors import CORS
 from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
 import os
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-app.register_blueprint(app_views)
 
 auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
 
-if AUTH_TYPE == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-elif AUTH_TYPE == 'auth':
+if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+elif AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+elif AUTH_TYPE == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -48,9 +52,7 @@ def before_request():
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
 
---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module for Session Authentication """
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ SessionAuth class that inherits from Auth """
+    pass
