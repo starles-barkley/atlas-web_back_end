ce6ef5d098c247b4f4067e41c03355c0b1cb5163
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-22 01:05:33 +0000

    pycodestyle;

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -5,10 +5,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def home():
     """Return a welcome message in JSON format."""
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
