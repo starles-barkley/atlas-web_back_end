c3b40211157352601953addb4643aacff1ee56ba
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-22 00:35:17 +0000

    revise user model

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
-"""User Model Module"""
+""" User Model Module """
 
-from sqlalchemy import Column, Integer, String
+
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
+
 
 class User(Base):
     """Represents a user in the users table."""
 
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
-
-    def __repr__(self):
-        """Return a string representation of the User object."""
-        return f"<User(id={self.id}, email={self.email})>"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
