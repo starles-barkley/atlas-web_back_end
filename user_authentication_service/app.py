abd28eff22817f8ed497feee008002c3d5877782
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-22 01:08:12 +0000

    add register new user method

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -12,5 +12,18 @@ def home():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user():
+    """Register a new user.
+    """
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
