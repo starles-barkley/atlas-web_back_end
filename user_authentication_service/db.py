e49b5f253ac7a4cf4ad27114f3d97bfae8862ce8
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-22 00:44:37 +0000

    import user and base from user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,8 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
-from typing import TypeVar
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+from sqlalchemy.exc import InvalidRequestError
+from user import Base, User
 
 from user import Base, User
 
@@ -42,3 +43,14 @@ class DB:
         self._session.commit()
         self._session.refresh(new_user)
         return new_user
+
+     def find_user_by(self, **kwargs) -> User:
+        """Find a user by the given keyword arguments.
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            raise NoResultFound("No user found matching the given criteria.")
+        except MultipleResultsFound:
+            raise InvalidRequestError("Multiple users found with the same criteria.")
