e0851353b82e1fd4291d0ff8447d5e4b40da13d6
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-22 00:21:24 +0000

    add hashbang and documentation;

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python3
+"""User Model Module"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class User(Base):
+    """Represents a user in the users table."""
+
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -13,4 +18,5 @@ class User(Base):
     reset_token = Column(String, nullable=True)
 
     def __repr__(self):
+        """Return a string representation of the User object."""
         return f"<User(id={self.id}, email={self.email})>"
