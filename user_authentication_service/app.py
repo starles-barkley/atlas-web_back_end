ba91445de05e5a5d159baa35ba5e1f884e7e633c
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-23 04:49:21 +0000

    create login route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -28,5 +28,20 @@ def register_user():
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ Login Route for User
+    """
+    email = request.form['email']
+    password = request.form['password']
+    if AUTH.valid_login(email, password):
+        AUTH.create_session(email)
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
