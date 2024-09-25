d6182ba5853cd724de370fae8d411a79433eb9b9
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-25 20:37:07 +0000

    add decorator

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,6 @@ import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
-
 class TestAccessNestedMap(unittest.TestCase):
     """Unit test for the access_nested_map function."""
 
@@ -17,10 +16,16 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map returns the expected value."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    def test_access_nested_map_exception(self, nest, path):
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
         """Test access_nested_map raises KeyError with correct message."""
-        with self.assertRaises(KeyError):
-            utils.access_nested_map(nest, path)
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        # Verify that the exception message is as expected
+        self.assertEqual(str(cm.exception), f"'{path[-1]}'")
 
 
 if __name__ == '__main__':
