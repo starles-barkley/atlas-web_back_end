acff390914e0e2d4d1ca0bf2994462d13496eb27
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-09-25 20:44:04 +0000

    define TestGetJson class and test for getJson method

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 '''Util Testing Module'''
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -27,6 +28,27 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
         self.assertEqual(str(cm.exception), f"'{path[-1]}'")
+
+
+class TestGetJson(unittest.TestCase):
+    """Unit test for the get_json function."""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test that get_json returns the expected payload."""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':
