5ed62443f99cf4244dd7b563849cc72bacdf0f52
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-10-06 23:07:51 +0000

    rewrite average_score script

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,22 +1,15 @@
 -- Create the ComputeAverageScoreForUser stored procedure
-DELIMITER $$
+delimiter $$
+CREATE PROCEDURE ComputeAverageScoreForUser(input_id INT)
+    BEGIN
+        DECLARE average FLOAT;
+        SELECT AVG(COALESCE(score)) INTO average
+        FROM corrections
+        WHERE user_id = input_id;
 
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
-BEGIN
-    DECLARE average_score DECIMAL(10, 2);
-
-    -- Compute the average score for the given user
-    SELECT AVG(score)
-    INTO average_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Update the average_score field in the users table
-    UPDATE users
-    SET average_score = average_score
-    WHERE id = user_id;
-END$$
-
-DELIMITER ;
+        UPDATE users
+        SET average_score = average
+        WHERE id = input_id;
+    END;
+$$
+delimiter ;
