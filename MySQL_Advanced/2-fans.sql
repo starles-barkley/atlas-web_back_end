d04590d0a0d388cd0807cbdead2f41ac0c1d8303
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-10-06 18:58:07 +0000

    reword and simplify the fans script

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,13 +1,5 @@
--- Ensure the table exists before proceeding
-SELECT 1 FROM metal_bands LIMIT 1;
-
 -- Query to rank country origins based on the total number of non-unique fans
-SELECT
-    origin,
-    SUM(nb_fans) AS total_fans
-FROM
-    metal_bands
-GROUP BY
-    origin
-ORDER BY
-    total_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY SUM(fans) DESC;
