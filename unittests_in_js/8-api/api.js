ccfa912dccc43d87f96a93b873cb0bce8a5cd9e3
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-10-23 23:35:57 +0000

    fix typo

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 7865;
 
 app.get('/', (req, res) => {
-    res.status(200).send('Welcome to the payment system');
+  res.send('Welcome to the payment system');
 });
 
-app.listen(port, () => {
-    console.log('API available on localhost port 7865');
-})
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,11 +1,18 @@
-const axios = require('axios');
-const { expect, assert } = require('chai');
+const request = require('request');
+const { expect } = require('chai');
 
-describe('API index page integration test', () => {
-    it('should return a successful status code and the landing message', () => {
-        axios.get('localhost:7865/').then((res) => {
-            assert.equal(res.status, 200);
-            assert.equal(res.data, 'Welcome to the payment system');
-        });
+describe('Index page', () => {
+  it('should return status code 200', (done) => {
+    request('http://localhost:7865/', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
     });
+  });
+
+  it('should return the correct message', (done) => {
+    request('http://localhost:7865/', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
 });
