b24d2ead0344646493723abec3bbb711d37b391b
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-10-23 20:27:20 +0000

    add utils module

---
+++ unittests_in_js/utils.js
@@ -0,0 +1,17 @@
+module.exports = Utils = {
+  calculateNumber: (type, a, b) => {
+    const roundA = Math.round(a);
+    const roundB = Math.round(b);
+    switch (type) {
+      case 'SUM':
+        return roundA + roundB;
+      case 'SUBTRACT':
+        return roundA - roundB;
+      case 'DIVIDE':
+        if (roundB === 0){
+          return 'Error';
+        }
+        return roundA / roundB;
+    }
+  }
+}
